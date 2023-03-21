2ceb9037485789d477ecc32c74123f69d2f682b9
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-03-21 15:52:19 +0100

    Use Devise for authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+#   :lockable, :timeoutable, :trackable and :omniauthable
+
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
     has_many :groups, class_name: 'Group', dependent: :destroy
     has_many :entities, class_name: 'Entity', foreign_key: 'author_id', dependent: :destroy
 
-    validates :full_name, presence: true
+    validates :name, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
-  resources :groups
-  resources :entities
+  devise_for :users
+
+  root "groups#index"
+
+  resources :groups, only: [:index, :show, :create, :destroy] do
+    resources :entities, only: [:index, :show, :create, :destroy]
+  end
+
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
