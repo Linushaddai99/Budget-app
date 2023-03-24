46819d6b057105b8f34c62b8084cf4794257208b
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2023-03-25 00:51:38 +0100

    Complete style and fix linter errors

--- app/controllers/entities_controller.rb
+++ app/controllers/entities_controller.rb
@@ -1,25 +1,14 @@
 class EntitiesController < ApplicationController
-  # before_action :set_entity, only: %i[show edit create new update destroy]
-
-  # GET /entities or /entities.json
   def index
     @group = Group.find(params[:group_id])
-    @entities = Entity.joins(:groups).where(groups: {id: params[:group_id]}).order(created_at: :desc)
+    @entities = Entity.joins(:groups).where(groups: { id: params[:group_id] }).order(created_at: :desc)
   end
 
-  # GET /entities/1 or /entities/1.json
-  def show; end
-
-  # GET /entities/new
   def new
     @groups = Group.where(user_id: current_user.id)
     @entity = Entity.new
   end
 
-  # GET /entities/1/edit
-  def edit; end
-
-  # POST /entities or /entities.json
   def create
     params = entity_params
     @entity = Entity.new(name: params[:name], amount: params[:amount])
@@ -39,59 +28,8 @@ class EntitiesController < ApplicationController
       render :new
     end
   end
-  
-  # def create
-  #   @entity = Entity.new(entity_params)
-
-  #   if @entity.save!
-  #     redirect_to group_entities_path(params[:group_id])
-  #   end
-
-    # respond_to do |format|
-    #   if @entity.save
-    #     format.html { redirect_to entity_url(@entity), notice: 'Entity was successfully created.' }
-    #     format.json { render :show, status: :created, location: @entity }
-    #   else
-    #     format.html { render :new, status: :unprocessable_entity }
-    #     format.json { render json: @entity.errors, status: :unprocessable_entity }
-    #   end
-    # end
-  # end
-
-  # PATCH/PUT /entities/1 or /entities/1.json
-  def update
-    respond_to do |format|
-      if @entity.update(entity_params)
-        format.html { redirect_to entity_url(@entity), notice: 'Entity was successfully updated.' }
-        format.json { render :show, status: :ok, location: @entity }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @entity.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /entities/1 or /entities/1.json
-  def destroy
-    @entity.destroy
-
-    respond_to do |format|
-      format.html { redirect_to entities_url, notice: 'Entity was successfully destroyed.' }
-      format.json { head :no_content }
-    end
-  end
 
   private
-
-  # Use callbacks to share common setup or constraints between actions.
-  def set_entity
-    @entity = Entity.find(params[:id])
-  end
-
-  # Only allow a list of trusted parameters through.
-  # def entity_params
-  #   params.require(:entity).permit(:name, :amount).merge(author: current_user)
-  # end
 
   def entity_params
     params.require(:entity).permit(:name, :amount, group_ids: [])
--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -47,8 +47,6 @@ class GroupsController < ApplicationController
     @group = Group.new
   end
 
-  def edit; end
-
   def create
     @group = Group.new(group_params)
     @group.save
@@ -57,18 +55,6 @@ class GroupsController < ApplicationController
       redirect_to '/'
     else
       render :new, alert: 'Error, Group not created'
-    end
-  end
-
-  def update
-    respond_to do |format|
-      if @group.update(group_params)
-        format.html { redirect_to group_url(@group), notice: 'Group was successfully updated.' }
-        format.json { render :show, status: :ok, location: @group }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @group.errors, status: :unprocessable_entity }
-      end
     end
   end
 
@@ -83,14 +69,7 @@ class GroupsController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_group
-    @group = Group.find(params[:user_id])
-  end
-
-  # Only allow a list of trusted parameters through.
   def group_params
-    # params.fetch(:group, {})
     params.require(:group).permit(:name, :icon).merge(user: current_user)
   end
 end
